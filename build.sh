fdc0011683c73d61bb0918378ad07c5ee90a5a08
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 16:09:57 -0700

    This should fix that

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ pwd
 ls -l
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
-bundle config local.contrast ../contrast
+bundle config local.contrast ../repo
 bundle install
 bundle exec jekyll build
