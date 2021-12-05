a121d6772fe67f141b1d7f688d6ae72c152b3152
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 16:33:12 -0700

    And one more thing

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+cat $HOME
+pwd
+ls -l
+cd ..
 pwd
 ls -l
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
+ls -l
 cd chrisfnicholson.github.io
-bundle config local.contrast ../repo
+ls -l ../repo
+bundle config local.contrast /opt/buildhome/repo
 bundle install
 bundle exec jekyll build
