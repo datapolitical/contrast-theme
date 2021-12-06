1ae7ec2ded4c7692734b8823c4b62e1be2259986
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 20:11:52 -0700

    TYPO

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 bundle clean
 bundle config set disable_local_branch_check true
 gem build contrast.gemspec
-gem install contrast-3.0.8.gem
+gem install contrast-3.0.28.gem
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
