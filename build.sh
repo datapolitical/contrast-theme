007c11f53a34ad4117db31d28189e8f5b76b1e51
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 17:52:16 -0700

    Something weird this way comes

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 cat $HOME
+cat $HOME/.bundle/config
 pwd
 ls -l
+cat .bundle/config
 cd ..
 pwd
 ls -l
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 ls -l
 cd chrisfnicholson.github.io
+cat .bundle/config
 ls -l /opt/buildhome/repo
-# bundle config local.contrast /opt/buildhome/repo
+bundle config local.contrast /opt/buildhome/repo
+cat $HOME/.bundle/config
 bundle install # --redownload
 bundle exec jekyll build
