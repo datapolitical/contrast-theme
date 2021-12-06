8e0bb78e203e55402b2fa3f3113934f0d18c827b
Chris Nicholson <datapolitical@gmail.com>
2021-12-06 11:08:56 -0700

    I think we may have fixed it

--- build.sh
+++ build.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 source ~/.rvm/scripts/rvm
 rvm 2.7.1 --default
-echo $GEM_PATH
-gem sources
-gem environment
-# git pull
 git branch master
 git checkout master
-# bundle config set disable_local_branch_check true
-# gem build contrast-theme.gemspec
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
 bundle config set local.contrast-theme $HOME/repo
-BUNDLE_GEMFILE=Gemfile-dev bundle install --verbose
-# gem uninstall contrast-theme
-# gem cleanup contrast-theme
-gem query --local
-# gem install jekyll-include-cache --source http://rubygems.org
-# gem install jekyll_picture_tag --source http://rubygems.org
-# gem install jekyll-pwa-workbox --source http://rubygems.org
-# gem install jekyll-sitemap --source http://rubygems.org
-echo $GEM_HOME
-# gem install --verbose --install-dir /opt/buildhome/.rvm/gems/ruby-2.7.1/ $HOME/repo/contrast*.gem 
-# echo $GEM_PATH
-BUNDLE_GEMFILE=Gemfile-dev bundle exec jekyll build --config _config.yml,_config_dev.yml
+bundle install --verbose
+bundle exec jekyll build --config _config.yml,_config_dev.yml
 cp -r gh-pages $HOME/repo/gh-pages
