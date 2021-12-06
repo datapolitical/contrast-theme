112c83fe3e0ea5bf7b00e6a76cbfc7af8e3c1b44
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 23:25:50 -0700

    Or maybe this!

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ cd chrisfnicholson.github.io
 gem environment
 bundle install --verbose --path /opt/buildhome/.rvm/gems/ruby-2.7.1
 gem query --local
-# gem install jekyll-include-cache --source http://rubygems.org
-# gem install jekyll_picture_tag --source http://rubygems.org
-# gem install jekyll-pwa-workbox --source http://rubygems.org
-# gem install jekyll-sitemap --source http://rubygems.org
+gem install jekyll-include-cache --source http://rubygems.org
+gem install jekyll_picture_tag --source http://rubygems.org
+gem install jekyll-pwa-workbox --source http://rubygems.org
+gem install jekyll-sitemap --source http://rubygems.org
 gem install $HOME/repo/*.gem
 bundle exec jekyll build
