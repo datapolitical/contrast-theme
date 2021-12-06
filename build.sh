abf4b3c03078f54db439763c1a2c3bae1427681f
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 19:00:25 -0700

    One more commitment

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 bundle config set disable_local_branch_check true
-gem install contrast -s https://github.com/datapolitical/contrast
-bundle config set local.contrast /opt/buildhome/repo
+bundle config set gemfile /opt/buildhome/chrisfnicholson.github.io/Gemfile
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
+gem install contrast -s https://github.com/datapolitical/contrast
+bundle config set local.contrast /opt/buildhome/repo
+bundle config set gemfile /opt/buildhome/chrisfnicholson.github.io/Gemfile
 bundle exec jekyll build
