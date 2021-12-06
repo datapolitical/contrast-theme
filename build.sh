2a63c23f9aa8d8e9a644831df379ea5d3cca8aa3
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 18:45:22 -0700

    Another try

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+bundle config set disable_local_branch_check true
+gem install contrast -s https://github.com/datapolitical/contrast
+bundle config set local.contrast /opt/buildhome/repo
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
