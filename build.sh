1c7e2a284f0821daecda97c05df658a2b4aee321
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 17:41:31 -0700

    Lets try this

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ git clone https://github.com/datapolitical/chrisfnicholson.github.io
 ls -l
 cd chrisfnicholson.github.io
 ls -l /opt/buildhome/repo
-bundle config local.contrast /opt/buildhome/repo
-bundle install --redownload
+# bundle config local.contrast /opt/buildhome/repo
+bundle install # --redownload
 bundle exec jekyll build
