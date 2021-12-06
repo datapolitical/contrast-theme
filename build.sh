c08e3b02b06b51aadd1012cd05506782db2948fe
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 22:36:21 -0700

    More debug stuff

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 gem sources
+gem environment
 bundle config set disable_local_branch_check true
 gem build contrast-theme.gemspec
 cd ..
+gem environment
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
+gem environment
 bundle install --redownload
 gem query --local
 gem -i jekyll-include-cache
