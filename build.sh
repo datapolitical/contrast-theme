11c1b72bcd1991fd5d567737de01b267e3ba69cd
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 21:10:57 -0700

    More changes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 gem uninstall contrast
 bundle config set disable_local_branch_check true
 gem build contrast-theme.gemspec
-gem install *.gem
+gem install --local *.gem
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
