f0d54c7f90440ea0fa72460f55c72ca51c45a7ec
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 18:31:25 -0700

    bundle config set local.contrast /opt/buildhome/repo

--- build.sh
+++ build.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-cat $HOME
-cat $HOME/.bundle/config
-pwd
-ls -l
-cat .bundle/config
 cd ..
-pwd
-ls -l
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
-ls -l
 cd chrisfnicholson.github.io
-cat .bundle/config
-ls -l /opt/buildhome/repo
-bundle config set local.contrast /opt/buildhome/repo
-cat $HOME/.bundle/config
-bundle install --redownload
 bundle exec jekyll build
