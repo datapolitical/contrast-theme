d642eb73a07d1bd2a9896db92904445cd6e64852
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 19:02:58 -0700

    And even more

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ bundle config set gemfile /opt/buildhome/chrisfnicholson.github.io/Gemfile
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
-gem install contrast -s https://github.com/datapolitical/contrast
+gem install contrast -s https://github.com/datapolitical/contrast.git
 bundle config set local.contrast /opt/buildhome/repo
 bundle config set gemfile /opt/buildhome/chrisfnicholson.github.io/Gemfile
 bundle exec jekyll build
