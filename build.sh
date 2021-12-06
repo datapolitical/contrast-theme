b95672c255c09e5fb3906f0338f78bb274c90ebc
Chris Nicholson <datapolitical@gmail.com>
2021-12-06 00:05:51 -0700

    Hmmm

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source ~/.rvm/scripts/rvm
+rvm 2.7.1
 gem sources
 gem environment
 bundle config set disable_local_branch_check true
@@ -8,11 +10,11 @@ gem environment
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
 gem environment
-bundle install --verbose --path /opt/buildhome/.rvm/gems/ruby-2.7.1
+bundle install --verbose
 gem query --local
-gem install jekyll-include-cache --source http://rubygems.org
-gem install jekyll_picture_tag --source http://rubygems.org
-gem install jekyll-pwa-workbox --source http://rubygems.org
-gem install jekyll-sitemap --source http://rubygems.org
+# gem install jekyll-include-cache --source http://rubygems.org
+# gem install jekyll_picture_tag --source http://rubygems.org
+# gem install jekyll-pwa-workbox --source http://rubygems.org
+# gem install jekyll-sitemap --source http://rubygems.org
 gem install $HOME/repo/*.gem
 bundle exec jekyll build
