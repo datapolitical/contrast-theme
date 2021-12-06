600cbb08dd285b6a9cf90e967c6760a09044a232
Chris Nicholson <datapolitical@gmail.com>
2021-12-06 05:58:53 -0700

    Maybe this time I'll be lucky

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ rvm 2.7.1 --default
 echo $GEM_PATH
 gem sources
 gem environment
+git branch
+git checkout $CF_PAGES_BRANCH
 bundle config set disable_local_branch_check true
 gem build contrast-theme.gemspec
 cd ..
@@ -19,7 +21,8 @@ gem query --local
 # gem install jekyll_picture_tag --source http://rubygems.org
 # gem install jekyll-pwa-workbox --source http://rubygems.org
 # gem install jekyll-sitemap --source http://rubygems.org
-gem install $HOME/repo/contrast*.gem 
+echo $GEM_HOME
+gem install --verbose --install-dir /opt/buildhome/.rvm/gems/ruby-2.7.1/ $HOME/repo/contrast*.gem 
 echo $GEM_PATH
 bundle exec jekyll build
 cp -r gh-pages $HOME/repo/gh-pages
