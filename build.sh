edfbccaa58f394ca5027aa959d153306ee41b502
Chris Nicholson <datapolitical@gmail.com>
2021-12-05 21:03:15 -0700

    This could work!

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 gem uninstall contrast
 bundle config set disable_local_branch_check true
 gem build contrast-theme.gemspec
-gem install contrast-theme-3.0.28.gem
+gem install *.gem
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
