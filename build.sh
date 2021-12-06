1c7634964a6935e81bb82071498b671039032bf5
Chris Nicholson <datapolitical@gmail.com>
2021-12-06 10:45:36 -0700

    and switching back to the second GEMfile

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ cd ..
 git clone https://github.com/datapolitical/chrisfnicholson.github.io
 cd chrisfnicholson.github.io
 bundle config set local.contrast-theme $HOME/repo
-bundle install --verbose
+BUNDLE_GEMFILE=Gemfile-dev bundle install --verbose
 # gem uninstall contrast-theme
 # gem cleanup contrast-theme
 gem query --local
@@ -24,5 +24,5 @@ gem query --local
 echo $GEM_HOME
 # gem install --verbose --install-dir /opt/buildhome/.rvm/gems/ruby-2.7.1/ $HOME/repo/contrast*.gem 
 # echo $GEM_PATH
-bundle exec jekyll build --config _config.yml,_config_dev.yml
+BUNDLE_GEMFILE=Gemfile-dev bundle exec jekyll build --config _config.yml,_config_dev.yml
 cp -r gh-pages $HOME/repo/gh-pages
