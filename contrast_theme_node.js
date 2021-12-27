0335c16c7a4bd8f62bebcea119c77301ef195a7b
Chris Nicholson <datapolitical@gmail.com>
2021-12-27 15:28:15 -0800

    even better!

--- contrast_theme_node.js
+++ contrast_theme_node.js
@@ -7,7 +7,17 @@ Crittr({
 })
     .then(({ critical, rest }) => {
         console.log(critical);
+        const fs = require('fs');
+        fs.writeFile('assets/css/generated-critical.css', critical, err => {
+          if (err) {
+            console.error(err)
+            return
+          }
+        })
     })
     .catch(err => {
         console.error(err);
     });
+    
+
+const content = 'Some content!'
