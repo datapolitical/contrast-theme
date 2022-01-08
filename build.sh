3962f247b11d26cdf10ecf1da4addab19e578fab
Chris Nicholson <datapolitical@gmail.com>
2022-01-07 16:37:48 -0800

    Updating some code [no-ci]

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ source ~/.rvm/scripts/rvm
 rvm default
 git branch master
 git checkout master
-cat _headers > assets/css/test-headers.txt
-echo -n "style-src " >> assets/css/test-headers.txt
-inline_hash=`echo `cat assets/css/generated-critical.css` | openssl dgst -binary -sha256 | base64 `
-echo -n "'$inline_hash'" >> assets/css/test-headers.txt
-echo -n ";" >> assets/css/test-headers.txt
+echo -n "/*\nContent-Security-Policy-Report-Only: default-src 'none'; " > _headers
+echo -n "style-src " >> _headers
+inline_hash=$(echo `cat assets/css/generated-critical.css` | openssl dgst -binary -sha256 | base64)
+echo -n "'$inline_hash'" >> _headers
+echo -n ";" >> _headers
 cd ..
 git clone https://github.com/datapolitical/chrisfnicholson
 cd chrisfnicholson
